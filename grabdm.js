cfef2fbfc2a639cf96ca1a1d0125fa060cb2ef18
xscamtrap <toni.rocci@proton.me>
2025-12-27 23:03:13 +0100

    Update grabdm.js
    I officially suck at Javascript..

--- grabdm.js
+++ grabdm.js
@@ -1,18 +1,20 @@
+
 var alldm = [];
+var intervalID = "";
 clearInterval(intervalID);
-var lastblock = ""
+var lastblock = "";
 document.querySelector('[data-testid^="dm-message-list-container"]').getElementsByTagName('div')[1].scrollTo(0,0);
-var intervalID = window.setInterval(myCallback, 1000);
+intervalID = window.setInterval(myCallback, 1000);
 
 function myCallback() 
 { document.querySelector('[data-testid^="dm-message-list-container"]').getElementsByTagName('div')[1].scrollBy(0,400);
-  var element = document.querySelector('[data-testid^="dm-message-list"]').getElementsByTagName('ul')[0].innerHTML;
-  if (lastblock === element)
+  var el = document.querySelector('[data-testid^="dm-message-list"]').getElementsByTagName('ul')[0].innerHTML;
+  if (lastblock.length == el.length)
 	{ clearInterval(intervalID);
       console.log("Exit");
 	  console.log(alldm);
     } else
-	{ alldm.push(element);
-      lastblock = element;
+	{ alldm.push(el);
+      lastblock = el;
     }
 }
